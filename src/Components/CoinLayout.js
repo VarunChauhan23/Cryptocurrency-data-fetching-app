104abd658a4d35a40c98e2db669c84ecfbf9b4d2
Varun Chauhan <agrasector@gmail.com>
2024-01-15 22:50:34 +0530

    Updated Responsiveness

--- src/Components/CoinLayout.js
+++ src/Components/CoinLayout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './Coin.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function CoinLayout({ image, name, price, market_cap, price_change }) {
     // here all image, name, price, market_cap, price_change are props
     // line 16 code ensures that if change is posotive then data will be gree otherwise red showing decrement or increment
     return (
         <div className="coin-container">
-            <div className="coin-row">
+            {/* <div className="coin-row">
                 <div className="coin">
                     <img src={image} alt="crypto" />
                     <h1>{name}</h1>
@@ -19,7 +21,24 @@ function CoinLayout({ image, name, price, market_cap, price_change }) {
                     Mkt Cap: Rs.{market_cap}
                 </p>
                 </div>
-            </div>
+            </div> */}
+            <Row id="coin-row">
+                <Col id="coin">
+                    <img src={image} alt="crypto" id="coin-image" />
+                    <h1 id="coin-name">{name}</h1>
+                </Col>
+                <Col>
+                    <p className="coin-price">Rs.{price}</p>
+                </Col>
+                <Col>
+                    {price_change < 0 ? (<p className="coin-percent red">{price_change}%</p>) : (<p className="coin-percent green">{price_change}%</p>)}
+                </Col>
+                <Col>
+                    <p className="coin-marketcap">
+                        Mkt Cap: Rs.{market_cap}
+                    </p>
+                </Col>
+            </Row>
         </div>
     )
 }
